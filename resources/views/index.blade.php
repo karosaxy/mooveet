03cb4747c63ccc664bfb6e90aea56dda4cd14cd3
karosaxy <araromirichard@gmail.com>
2020-04-25 01:15:09 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,333 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('./css/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('./css/css/all.min.css')}}">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link rel="stylesheet" href="{{ asset('./css/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('./css/style.css')}}">
+    <title>Homepage</title>
+</head>
+
+<body>
+
+
+
+
+
+
+    <!-- Main navigation -->
+    <header>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-dark navbar-collapse">
+            <a class="navbar-brand text-white" href="#">
+                <strong>MOOVEET</strong></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link pr-3" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link border-left border-white pl-3" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item border-left border-white pl-3">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item border-left border-white pl-3">
+                        <a class="nav-link" href="#">Driver Scan</a>
+                    </li>
+                    <li class="nav-item border-left border-white pl-3">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+                </ul>
+            </div>
+            <form class="form-inline justify-content-end ">
+                <div class="">
+                    <input class="form-control mr-sm-2 text-white bg-transparent" type="text" placeholder="Search" aria-label="Search">
+                </div>
+            </form>
+        </nav>
+        <!-- Full Page Intro -->
+        <div class="view" style="background-image: url('img/first.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-gradient d-flex justify-content-center align-items-center">
+                <!-- Content -->
+                <div class="container">
+                    <!--Grid row-->
+                    <div class="row">
+                        <!--Grid column-->
+                        <div class="col-md-6 col-sm-12 text-white text-center text-md-left mt-xl-5 mb-5 wow fadeInLeft" data-wow-delay="0.3s">
+                            <h1 class="h1-responsive font-weight-bold mt-sm-5">Make your World go round</h1>
+                            <hr class="hr-light">
+                            <h6 class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga nesciunt dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea dolor molestiae iste.
+                            </h6>
+                            <a class="btn btn-light" type="button">Register</a>
+                            <a class="btn btn-outline-light btn-scan" type="button">Scan For A Driver</a>
+                            <a class="btn btn-light" type="button">Login</a>
+                        </div>
+                        <!--Grid column-->
+                        <!--Grid column-->
+                        <div class="col-md-6 col-sm-12 col-xl-5 mt-xl-5 wow fadeInRight" data-wow-delay="0.3s">
+                            <img src="img/lorry-tracker.png" alt="" class="img-fluid animated slideInRight">
+                        </div>
+                        <!--Grid column-->
+                    </div>
+                    <!--Grid row-->
+                </div>
+                <!-- Content -->
+            </div>
+            <!-- Mask & flexbox options-->
+        </div>
+        <!-- Full Page Intro -->
+    </header>
+    <!-- Main navigation -->
+    <!--Main Layout-->
+    <main>
+        <div class="container">
+            <!--Grid row-->
+            <div class="row py-5">
+                <!--Grid column-->
+                <div class="col-md-12 text-justify">
+                    <h2>Our Services</h2>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <!--Grid column-->
+            </div>
+
+            <!-- Start of Card deck -->
+            <div class="mb-5 card-deck row">
+
+                <!-- Card -->
+                <div class="card mb-4 col-md-4 col-sm-12 shadow">
+
+                    <!--Card image-->
+                    <div class="view overlay">
+                        <img class="card-img-top" src="img/card1.jpg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+
+                    <!--Card content-->
+                    <div class="card-body">
+
+                        <!--Title-->
+                        <h4 class="card-title">WareHouse</h4>
+                        <!--Text-->
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                        <button type="button" class="btn btn-light-blue">Read more</button>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
+                <!-- Card -->
+                <div class="card mb-4 col-md-4 col-sm-12 shadow">
+
+                    <!--Card image-->
+                    <div class="view overlay">
+                        <img class="card-img-top" src="img/card4.jpg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+
+                    <!--Card content-->
+                    <div class="card-body">
+
+                        <!--Title-->
+                        <h4 class="card-title">Tracking</h4>
+                        <!--Text-->
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                        <button type="button" class="btn btn-light-blud">Read more</button>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
+                <!-- Card -->
+                <div class="card mb-4 col-md-4 col-sm-12 shadow">
+
+                    <!--Card image-->
+                    <div class="view overlay">
+                        <img class="card-img-top" src="img/card3.jpg" alt="Card image cap">
+                        <a href="#!">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>
+
+                    <!--Card content-->
+                    <div class="card-body">
+
+                        <!--Title-->
+                        <h4 class="card-title">Find A Truck</h4>
+                        <!--Text-->
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                        <button type="button" class="btn btn-light-blud">Read more</button>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
+            </div>
+            <!-- Card deck -->
+        </div>
+
+    </main>
+    <!--End of Main Layout-->
+
+
+    <!--Start of Footer -->
+    <footer class="mt-5 page-footer font-small indigo">
+
+        <!-- Footer Links -->
+        <div class="container">
+
+            <!-- Grid row-->
+            <div class="row text-center text-white d-flex justify-content-center pt-5 mb-3">
+
+                <!-- Grid column -->
+                <div class="col-md-2 mb-3">
+                    <h6 class="text-uppercase text-white font-weight-bold">
+                        <a href="#!">About us</a>
+                    </h6>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 mb-3">
+                    <h6 class="text-uppercase text-white font-weight-bold">
+                        <a href="#!">Services</a>
+                    </h6>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 mb-3">
+                    <h6 class="text-uppercase text-white font-weight-bold">
+                        <a href="#!">Awards</a>
+                    </h6>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 mb-3">
+                    <h6 class="text-uppercase text-white font-weight-bold">
+                        <a href="#!">Contact</a>
+                    </h6>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row-->
+            <hr class="rgba-white-light" style="margin: 0 15%;">
+
+            <!-- Grid row-->
+            <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+                <!-- Grid column -->
+                <div class="col-md-8 col-12 mt-5">
+                    <p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                        voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row-->
+            <hr class="rgba-white-light" style="margin: 0 15%;">
+
+            <!-- Grid row-->
+            <div class="row pt-4 pb-3">
+
+                <!-- Grid column -->
+                <div class="col-sm-12 text-center">
+
+                    <div class="mb-5 flex-center">
+
+                        <!-- Facebook -->
+                        <a href="#" class="fb-ic">
+                            <i class="fab fa-facebook-f text-white mr-4"></i>
+                        </a>
+                        <!-- Twitter -->
+                        <a href="#" class="tw-ic">
+                            <i class="fab fa-twitter fa-lg text-white mr-4"> </i>
+                        </a>
+                        <!-- Google +-->
+                        <a href="#" class="gplus-ic">
+                            <i class="fab fa-google-plus-g fa-lg text-white mr-4"> </i>
+                        </a>
+                        <!--Linkedin -->
+                        <a href="#" class="li-ic">
+                            <i class="fab fa-linkedin-in fa-lg text-white mr-4"> </i>
+                        </a>
+                        <!--Instagram-->
+                        <a href="#" class="ins-ic">
+                            <i class="fab fa-instagram fa-lg text-white mr-4"> </i>
+                        </a>
+                        <!--Pinterest-->
+                        <a href="#" class="pin-ic">
+                            <i class="fab fa-pinterest fa-lg text-white"> </i>
+                        </a>
+
+                    </div>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row-->
+
+        </div>
+        <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="mooveet.test/"> mooveet.test</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!--End of Footer -->
+
+    <!-- script -->
+
+    <!-- JQuery -->
+    <script src="{{ url('css/js/jquery-3.5.0.min.js') }}"></script>
+    <!-- popper -->
+    <script src="{{ url('css/js/popper.min.js') }}"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ url('css/js/bootstrap.min.js') }}"></script>
+    <script>
+        $(window).scroll(function() {
+            $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
+        });
+    </script>
+</body>
+
+</html>
